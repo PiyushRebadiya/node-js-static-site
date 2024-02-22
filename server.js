d83e8f34551ff0f02fdbba9939b2d3bfe85a3ed7
Piyush Rebadiya <piyushrebadiya123@gmail.com>
2024-02-22 16:26:17 +0530

    chnages add

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const jsonData = {
   message: 'Hello, World!',
   items: [
     { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-    { id: 4, name: 'Item 4' }
   ]
 };
 
